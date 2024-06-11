27b3917ee81c29da91fd921a04c8cfeaf9ea8aa0
Jorgen Bengtsson <jorgen@pm-production.nu>
2024-06-11 23:15:42 +0200

    WiP: Following an Example

--- main.js
+++ main.js
@@ -1,45 +1,96 @@
-import "./style.css";
+// Following Example: https://github.com/phaserjs/phaser-by-example/blob/main/runner/src/scenes/game.js
+
 import Phaser from "phaser";
 
-class Example extends Phaser.Scene {
-  preload() {
-    this.load.setBaseURL("https://labs.phaser.io");
+import Player from "./player";
 
-    this.load.image("sky", "assets/skies/space3.png");
-    this.load.image("logo", "assets/sprites/phaser3-logo.png");
-    this.load.image("red", "assets/particles/red.png");
+import "./style.css";
+
+const TILE_WIDTH = 25;
+const TILE_HEIGHT = 25;
+
+const level = [
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+];
+
+class Game extends Phaser.Scene {
+  constructor() {
+    super({ key: "game" });
+    this.player = null;
+  }
+
+  init(data) {}
+
+  preload() {
+    this.load.image("tileset", "assets/tile-set.25x25.png");
   }
 
   create() {
-    this.add.image(400, 300, "sky");
-
-    const particles = this.add.particles(0, 0, "red", {
-      speed: 100,
-      scale: { start: 1, end: 0 },
-      blendMode: "ADD",
+    this.map = this.make.tilemap({
+      data: level,
+      tileWidth: TILE_WIDTH,
+      tileHeight: TILE_HEIGHT,
     });
 
-    const logo = this.physics.add.image(400, 100, "logo");
+    const tiles = this.map.addTilesetImage("tileset");
+    const tileLayer = this.map.createLayer(0, tiles, 0, 0);
 
-    logo.setVelocity(100, 200);
-    logo.setBounce(1, 1);
-    logo.setCollideWorldBounds(true);
+    this.map.setCollision([1, 2]);
 
-    particles.startFollow(logo);
+    this.player = new Player(this, 100, 100);
+    this.SPACE = this.input.keyboard.addKey(
+      Phaser.Input.Keyboard.KeyCodes.SPACE
+    );
+
+    this.physics.add.collider(
+      this.player,
+      tileLayer,
+      () => {
+        return true;
+      },
+      this
+    );
   }
+
+  // update() {
+  //   if (Phaser.Input.Keyboard.JustDown(this.SPACE)) {
+  //     this.jump();
+  //   } else if (this.player.body.blocked.down) {
+  //     this.jumpTween?.stop();
+  //     this.player.rotation = 0;
+  //   }
+  // }
 }
 
 const config = {
   type: Phaser.AUTO,
   width: 800,
   height: 600,
-  scene: Example,
+
   physics: {
     default: "arcade",
     arcade: {
       gravity: { y: 200 },
     },
   },
+  scene: Game,
 };
 
 const game = new Phaser.Game(config);
